90cb2bbeb876a8e6764172e9c16420f1be440deb
R.sewda <98726121+RaghavSewda@users.noreply.github.com>
2024-04-23 11:59:50 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,11 @@
+const marvel_heros = ['Thor', 'Ironman', 'Spiderman']
+const dc_heros = ['Superman', 'Batman', 'Flash']
+
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);
+
